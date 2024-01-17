030cff12b854e0ba1dca090a2f8b6c01095f72c9
Henry Munoz <alexcol123@yahoo.com>
2024-01-17 15:44:02 -0500

    12 commit

--- app/(pages)/product/[productId]/Share.js
+++ app/(pages)/product/[productId]/Share.js
@@ -1,6 +1,21 @@
 // Next share
 import { FacebookShareButton, FacebookIcon } from 'next-share'
 
+export const metadata = {
+  title: 'Handbag Store',
+  description: 'Handbags at the best prices buy today',
+  metadataBase: new URL('https://handbagstore-test-3.vercel.app/product/1'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en-US': '/en-US',
+    },
+  },
+  openGraph: {
+    images: '../../../img1.jpg',
+  },
+}
+
 const Share = () => {
   return (
     <div>

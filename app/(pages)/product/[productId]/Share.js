a925debb3e1760d4617515e4e490b2a80f9df5c8
Henry Munoz <alexcol123@yahoo.com>
2024-01-17 18:15:25 -0500

    25  commit

--- app/(pages)/product/[productId]/Share.js
+++ app/(pages)/product/[productId]/Share.js
@@ -1,5 +1,22 @@
 // Next share
-import { FacebookShareButton, FacebookIcon } from 'next-share'
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  PinterestShareButton,
+  PinterestIcon,
+  RedditShareButton,
+  RedditIcon,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappShareButton,
+  WhatsappIcon,
+  LinkedinShareButton,
+  LinkedinIcon,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
+  EmailShareButton,
+  EmailIcon,
+} from 'next-share'
 
 const Share = () => {
   return (
@@ -13,6 +30,70 @@ const Share = () => {
       >
         <FacebookIcon size={32} round />
       </FacebookShareButton>
+
+      <PinterestShareButton
+        url={
+          'https://handbagstore-test-3.vercel.app/product/64a4ebe300900d44bb50628a'
+        }
+        media={'next-share is a social share buttons for your next React apps.'}
+      >
+        <PinterestIcon size={32} round />
+      </PinterestShareButton>
+
+      <RedditShareButton
+        url={
+          'https://handbagstore-test-3.vercel.app/product/64a4ebe300900d44bb50628a'
+        }
+        title={'next-share is a social share buttons for your next React apps.'}
+      >
+        <RedditIcon size={32} round />
+      </RedditShareButton>
+
+      <TwitterShareButton
+        url={
+          'https://handbagstore-test-3.vercel.app/product/64a4ebe300900d44bb50628a'
+        }
+        title={'next-share is a social share buttons for your next React apps.'}
+      >
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+
+      <WhatsappShareButton
+        url={
+          'https://handbagstore-test-3.vercel.app/product/64a4ebe300900d44bb50628a'
+        }
+        title={'next-share is a social share buttons for your next React apps.'}
+        separator=':: '
+      >
+        <WhatsappIcon size={32} round />
+      </WhatsappShareButton>
+
+      <LinkedinShareButton
+        url={
+          'https://handbagstore-test-3.vercel.app/product/64a4ebe300900d44bb50628a'
+        }
+      >
+        <LinkedinIcon size={32} round />
+      </LinkedinShareButton>
+
+      <FacebookMessengerShareButton
+        url={
+          'https://handbagstore-test-3.vercel.app/product/64a4ebe300900d44bb50628a'
+        }
+        appId={''}
+      >
+        <FacebookMessengerIcon size={32} round />
+      </FacebookMessengerShareButton>
+
+      <EmailShareButton
+        url={
+          'https://handbagstore-test-3.vercel.app/product/64a4ebe300900d44bb50628a'
+        }
+        subject={'Next Share'}
+        body='body'
+      >
+        <EmailIcon size={32} round />
+      </EmailShareButton>
     </div>
   )
 }

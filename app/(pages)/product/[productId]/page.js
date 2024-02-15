faae60a1d6d73f59e79c424dbab441df0ba8edd8
Henry Munoz <alexcol123@yahoo.com>
2024-02-15 12:58:30 -0500

    Added functionality for metadata and ogimages

--- app/(pages)/product/[productId]/page.js
+++ app/(pages)/product/[productId]/page.js
@@ -1,3 +1,5 @@
+import { Metadata, ResolvingMetadata } from 'next'
+
 import ListRating from './ListRating'
 import ProductDetails from './ProductDetails'
 import getProductsById from '../../../../actions/getProductById'
@@ -10,19 +12,41 @@ export const dynamic = 'force-dynamic'
 
 // MORE ABOUT METADATA https://www.youtube.com/shorts/MYU_7RK3RHM
 
-export const metadata = {
-  title: 'Handbag Store',
-  description: 'Handbags at the best prices buy today',
-  metadataBase: new URL('https://handbagstore-test-3.vercel.app'),
+export async function generateMetadata({ params, searchParams }) {
+  // read route params
+  const id = params.productId
 
-  openGraph: {
-    images: [
-      {
-        url: 'https://images.pexels.com/photos/1204464/pexels-photo-1204464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      },
-    ],
-  },
+  // fetch data
+  // const product = await fetch(`https://.../${id}`).then((res) => res.json())
+
+  const product = await getProductsById(id)
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: product.name,
+    description: product.description,
+    openGraph: {
+      images: product.images[0].image,
+    },
+  }
 }
+
+// export const metadata = {
+//   title: 'Handbag Store',
+//   description:
+//     'Handbags at the best prices buy today, online shop for jet set luxury designer handbags watches shoes clothing and more',
+//   metadataBase: new URL('https://handbagstore-test-3.vercel.app'),
+
+//   openGraph: {
+//     images: [
+//       {
+//         url: 'https://images.pexels.com/photos/1204464/pexels-photo-1204464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+//       },
+//     ],
+//   },
+// }
 
 const ProductPage = async ({ params }) => {
   let productId = params.productId
@@ -30,9 +54,13 @@ const ProductPage = async ({ params }) => {
 
   const product = await getProductsById(productId)
 
+  console.log(
+    'product ========================================================================================================================================================================================================================      '
+  )
+
+  console.log(product)
+
   const relatedProducts = await getRelatedProducts(productId, product.category)
-
-
 
   if (!product || product === undefined)
     return (
